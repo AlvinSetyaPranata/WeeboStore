57509e79d833544af92ef18b244089f4592e0126
AlvinSetyaPranata <comand.alvin@gmail.com>
2023-11-19 10:04:28 +0700

    Created CartCard and Subtotal in Cart Page

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -5,6 +5,9 @@ import Navbar from '@/components/Navbar'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>Weebostore</title>
+      </head>
       <body>
           <Navbar />
           {children}
---
+++ frontend/src/components/Atoms/CartCard.jsx
@@ -0,0 +1,32 @@
+import TrashIcon from "../SVG/TrashIcon"
+
+
+export default function CartCard() {
+    return (
+
+        <div className="w-full px-2 py-1 pb-4 flex gap-4 border-b-2 border-gray-100 mb-4">
+            <div className="flex items-center">
+                <TrashIcon color="red" />
+                <img src="/card1.png" alt="example" className="w-[120px] h-[120px] ml-3"/>
+            </div>
+            <div className="flex flex-col justify-between">
+                <div>
+
+                    <label className="font-semibold text-lg">Jaket cosplay asuna - Sword Art online</label>
+                    <p className="text-green-600 text-sm">Stock Available</p>
+                </div>
+
+                <div className="flex justify-between w-full">
+                    <p className="font-bold">Rp.300.000</p>
+
+                    <div className="flex gap-2 items-center">
+                        <div>-</div>
+                        <input type="text" className="border-2 border-gray-300  rounded-lg py-0.5 max-w-[80px] text-sm text-center foont-semibold" />
+                        <div>+</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}

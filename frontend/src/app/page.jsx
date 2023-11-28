968cc92fd892ffba93f4a611c6514970558439f9
AlvinSetyaPranata <comand.alvin@gmail.com>
2023-11-28 09:30:11 +0700

    Removed prefetch function

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,4 +1,3 @@
-import { imageUrlResolver } from "@/utils/imageUrlResolver"
 import Home from "./content"
 
 export const metadata = {
@@ -6,23 +5,7 @@ export const metadata = {
 }
 
 
-export async function getData() {
-  const res = await fetch('http://127.0.0.1:8000/api/products')
-  const data = await res.json()
-
-  return data
-}
-
-
-
-export default async function Page() {
-  const data = await getData()
-
-  const newData = data.map((field) => {
-    field.product_image = imageUrlResolver(field.product_image)
-
-    return field
-  })
+export default function Page() {
 
   return <Home data={newData} />
 }
--- frontend/src/utils/imageUrlResolver.js
+++
@@ -1,3 +0,0 @@
-export function imageUrlResolver(image_path) {
-    return `http://127.0.0.1:8000${image_path}`
-}
